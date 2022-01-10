3ac35bacd85a461120bbc937d3b3804fc1f34506
DESKTOP-88FAOV3\EdsoLabs <>
2022-01-10 17:52:17 +0700

    update code

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
-    export const nftMarketAddress = "0xA3e172E147D2Ae3705a9143Ad6e2Cdf9fBD5D5CB"
-    export const nftAddress = "0xf2e4d7B536521214A792E475c729f9B9638269Ed"
-    export const tokenAddress = "0xB20f7C07AEd3C616c1C5aB9bA888c1bAb926031a"
-    export const walletAddress = "0x1c5DdDCF276E24b25E15B8637ea12988022B2218"
-    export const verifyAddress = "0xa62C37d39d5afEb0c4BD28D259F4F26b358638Ce"
+    export const nftMarketAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+    export const nftAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+    export const tokenAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
+    export const walletAddress = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"
+    export const verifyAddress = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"
   
